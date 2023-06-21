e0ae83ad82961754f388f6a1f9d6a0d294bfbc10
Alvaro Casas <alvaro.casas@estudiantat.upc.edu>
2023-06-21 16:25:18 +0200

    Seems to be working now

--- src/components/BeerSearch copy.jsx
+++ src/components/BeerSearch copy.jsx
@@ -1,37 +1,40 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { loadWord } from "../api";
 
 function MyComponent() {
   const [userInput, setUserInput] = useState("");
-  const [savedValue, setSavedValue] = useState("");
+  const [word, setSavedValue] = useState("");
   const [allInfo, setAllInfo] = useState(null);
 
+    
   const handleChange = (event) => {
     setUserInput(event.target.value);
   };
-  const saveValue = async() => {
+  const saveValue = async () => {
     setSavedValue(userInput);
     setUserInput("");
-    const all_info = await loadWord(savedValue);
-   setAllInfo(result);
-  
+    const all_info = await loadWord(userInput);
+    console.log(allInfo);
+    setAllInfo(all_info);
+
   };
-  
+
 
 
   return (
     <div>
       <input type="text" value={userInput} onChange={handleChange} />
       <button onClick={saveValue}>Search</button>
-      <p>Tu entrada: {savedValue}</p>
+      <p>Tu entrada: {word}</p>
       {allInfo && <p>Tu salida: {allInfo}</p>}
     </div>
-      
 
-  
-    
 
-    
+
+
+
+
+
   );
 }
 
