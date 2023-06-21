ec4064446ddef0a0966c2ca61d4f736c05299dea
Alvaro Casas <alvaro.casas@estudiantat.upc.edu>
2023-06-21 14:53:47 +0200

    Save the variable introduced

--- src/components/BeerSearch copy.jsx
+++ src/components/BeerSearch copy.jsx
@@ -1,19 +1,27 @@
 import React, { useState } from "react";
+import { loadWord } from "../api";
 
 function MyComponent() {
   const [userInput, setUserInput] = useState("");
+  const [savedValue, setSavedValue] = useState("");
 
   const handleChange = (event) => {
     setUserInput(event.target.value);
+  };
+  const saveValue = () => {
+    setSavedValue(userInput);
+    setUserInput("");
   };
 
   return (
     <div>
       <input type="text" value={userInput} onChange={handleChange} />
-      <button>Search</button>
-      <p>Tu entrada: {userInput}</p>
+      <button onClick={saveValue}>Search</button>
+      <p>Tu entrada: {savedValue}</p>
       
+
     </div>
+    
   );
 }
 
