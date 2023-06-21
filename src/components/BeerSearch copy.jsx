6c5f0fc771c9adbce15e5fc4c93effdfee03ceef
Alvaro Casas <alvaro.casas@estudiantat.upc.edu>
2023-06-21 15:37:22 +0200

    Trying to obtain the output from introducing word

--- src/components/BeerSearch copy.jsx
+++ src/components/BeerSearch copy.jsx
@@ -4,25 +4,36 @@ import { loadWord } from "../api";
 function MyComponent() {
   const [userInput, setUserInput] = useState("");
   const [savedValue, setSavedValue] = useState("");
+  const [allInfo, setAllInfo] = useState(null);
 
   const handleChange = (event) => {
     setUserInput(event.target.value);
   };
-  const saveValue = () => {
+  const saveValue = async() => {
     setSavedValue(userInput);
     setUserInput("");
+    const all_info = await loadWord(savedValue);
+   setAllInfo(result);
+  
   };
+  
+
 
   return (
     <div>
       <input type="text" value={userInput} onChange={handleChange} />
       <button onClick={saveValue}>Search</button>
       <p>Tu entrada: {savedValue}</p>
+      <p>Tu salida: {allInfo}</p>
       
 
     </div>
     
+
+    
   );
 }
+
+
 
 export default MyComponent;
